ae88c66ab9e467c64be9dd1ab6ce2fd04d747d8b
ProhorovArtu4anskii <kobelevadara210@mail.com>
2025-03-10 14:12:50 +0500

    Проверка каптчи и отправка на Сервер Умудов

--- login.php
+++ login.php
@@ -58,66 +58,69 @@
 		</div>
 		
 		<script>
-			function LogIn() {
-				var loading = document.getElementsByClassName("loading")[0];
-				var button = document.getElementsByClassName("button")[0];
-				
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
-				loading.style.display = "block";
-				button.className = "button_diactive";
-				
-				var data = new FormData();
-				data.append("login", _login);
-				data.append("password", _password);
-				
-				// AJAX запрос
-				$.ajax({
-					url         : 'ajax/login_user.php',
-					type        : 'POST', // важно!
-					data        : data,
-					cache       : false,
-					dataType    : 'html',
-					// отключаем обработку передаваемых данных, пусть передаются как есть
-					processData : false,
-					// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
-					contentType : false, 
-					// функция успешного ответа сервера
-					success: function (_data) {
-						console.log("Авторизация прошла успешно, id: " +_data);
-						if(_data == "") {
-							loading.style.display = "none";
-							button.className = "button";
-							alert("Логин или пароль не верный.");
-						} else {
-							localStorage.setItem("token", _data);
-							location.reload();
-							loading.style.display = "none";
-							button.className = "button";
-						}
-					},
-					// функция ошибки
-					error: function( ){
-						console.log('Системная ошибка!');
-						loading.style.display = "none";
-						button.className = "button";
-					}
-				});
-			}
-			
-			function PressToEnter(e) {
-				if (e.keyCode == 13) {
-					var _login = document.getElementsByName("_login")[0].value;
-					var _password = document.getElementsByName("_password")[0].value;
-					
-					if(_password != "") {
-						if(_login != "") {
-							LogIn();
-						}
-					}
-				}
-			}
-			
-		</script>
+    function LogIn() {
+        var loading = document.getElementsByClassName("loading")[0];
+        var button = document.getElementsByClassName("button")[0];
+
+        var _login = document.getElementsByName("_login")[0].value;
+        var _password = document.getElementsByName("_password")[0].value;
+
+        loading.style.display = "block";
+        button.className = "button_diactive";
+
+        var data = new FormData();
+        data.append("login", _login);
+        data.append("password", _password);
+
+        // Получаем ответ от reCAPTCHA перед отправкой данных
+        var captcha = grecaptcha.getResponse();
+        if (captcha.length > 0) { // Исправлено 'lenght' на 'length'
+            data.append('g-recaptcha-response', captcha);
+
+            // AJAX запрос
+            $.ajax({
+                url: 'ajax/login_user.php',
+                type: 'POST',
+                data: data,
+                cache: false,
+                dataType: 'html',
+                processData: false,
+                contentType: false,
+                success: function (_data) {
+                    console.log("Авторизация прошла успешно, id: " + _data);
+                    loading.style.display = "none";
+                    button.className = "button";
+
+                    if (_data === "") {
+                        alert("Логин или пароль неверный.");
+                    } else {
+                        localStorage.setItem("token", _data);
+                        location.reload();
+                    }
+                },
+                error: function () {
+                    console.log('Системная ошибка!');
+                    loading.style.display = "none";
+                    button.className = "button";
+                }
+            });
+        } else {
+            alert("Пожалуйста, подтвердите, что вы не робот."); // Добавлено сообщение, если капча не пройдена
+            loading.style.display = "none";
+            button.className = "button";
+        }
+    }
+
+    function PressToEnter(e) {
+        if (e.keyCode === 13) { // Используем строгое сравнение
+            var _login = document.getElementsByName("_login")[0].value;
+            var _password = document.getElementsByName("_password")[0].value;
+
+            if (_login !== "" && _password !== "") { // Упрощена проверка
+                LogIn();
+            }
+        }
+    }
+</script>
 	</body>
 </html>
